6c365f9c2e97c9638ef8508e720e29c800ae5dda
Nima-1376 <nima.ysfy@gmail.com>
2025-07-30 04:44:44 -0400

    Loop through image filenames to create thumbnail elements dynamically

--- assinment4/js/p2.js
+++ assinment4/js/p2.js
@@ -3,10 +3,10 @@ const thumbBar = document.querySelector('.thumb-bar');
 const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
-// 1. Declare array of filenames
+//  Declare array of filenames
 const imageFilenames = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
 
-// 2. Declare alt text object
+// Declare alt text object
 const imageAlts = {
   'pic1.jpg': 'Closeup of a blue human eye',
   'pic2.jpg': 'Rock that looks like a wave',
@@ -14,3 +14,10 @@ const imageAlts = {
   'pic4.jpg': 'Section of wall from a pharaoh’s tomb',
   'pic5.jpg': 'Large moth on a leaf'
 };
+
+//  Loop through images and generate thumbnails
+imageFilenames.forEach(filename => {
+  const newImage = document.createElement('img');
+  newImage.setAttribute('src', images/${filename});
+  newImage.setAttribute('alt', imageAlts[filename]);
+  thumbBar.appendChild(newImage);
