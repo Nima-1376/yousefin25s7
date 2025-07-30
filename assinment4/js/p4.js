a5c8284bb3f4809fa1ff3f151576d872cd1cea69
Nima-1376 <nima.ysfy@gmail.com>
2025-07-30 07:25:16 -0400

    Implement ball count display and update when balls are removed

--- assinment4/js/p4.js
+++ assinment4/js/p4.js
@@ -124,23 +124,22 @@ class EvilCircle extends Shape {
     }
   }
 }
+// Create balls
 const balls = [];
-
 while (balls.length < 25) {
   const size = random(10, 20);
   const ball = new Ball(
-    // ball position always drawn at least one ball width
-    // away from the edge of the canvas, to avoid drawing errors
-    random(0 + size, width - size),
-    random(0 + size, height - size),
+    random(size, width - size),
+    random(size, height - size),
     random(-7, 7),
     random(-7, 7),
     randomRGB(),
     size
   );
-
   balls.push(ball);
+  ballCount++;
 }
+ballCountPara.textContent = `Ball count: ${ballCount}`;
 
 function loop() {
   ctx.fillStyle = "rgba(0, 0, 0, 0.25)";
